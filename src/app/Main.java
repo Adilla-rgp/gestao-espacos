9ea1b70a577a5fcf6387c4f9507d61b04bfe7264
Kelvin-sants <kelvinvini75@gmail.com>
2025-07-28 15:18:13 -0300

    fix: corrigindo bugs

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,9 +1,12 @@
 package app;
 import view.*;
-import controller.app.AplicacaoController;;
+import controller.app.AplicacaoController;
+import database.DatabaseInitializer;
+ 
 
 public class Main {
      public static void main(String[] args) {
+        DatabaseInitializer.init();
         new AplicacaoController();
     }
 }
